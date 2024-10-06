83d64d07bb3315623ed9ea88998d357c270fad29
Abhishek Saxena <5abhisheksaxena@gmail.com>
2024-10-06 18:27:40 +0530

    Fixing write issue in the destination for the new excel file.

--- app/src/main/java/in/developingdeveloper/timeline/core/utils/export/excel/DefaultEventFileExporter.kt
+++ app/src/main/java/in/developingdeveloper/timeline/core/utils/export/excel/DefaultEventFileExporter.kt
@@ -1,34 +1,48 @@
 package `in`.developingdeveloper.timeline.core.utils.export.excel
 
+import android.content.Context
 import android.net.Uri
+import android.provider.DocumentsContract
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.apache.poi.ss.usermodel.Workbook
-import java.io.File
-import java.io.FileOutputStream
 import java.io.IOException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
-class DefaultEventFileExporter @Inject constructor() : EventFileExporter {
+class DefaultEventFileExporter @Inject constructor(
+    @ApplicationContext
+    val context: Context,
+) : EventFileExporter {
     @Suppress("BlockingMethodInNonBlockingContext")
     override suspend fun export(destination: Uri, workbook: Workbook): Result<String> {
         return withContext(Dispatchers.IO) {
             try {
-                val rootPath = destination.path
-                    ?: throw IllegalArgumentException("Destination root path is null.")
-
-                val root = File(rootPath)
+                val contentResolver = context.contentResolver
 
                 val fileName = generateFileName()
-                val excelOutputFilePath = File(root, fileName)
 
-                FileOutputStream(excelOutputFilePath)
+                val destinationFolderUri = DocumentsContract.buildDocumentUriUsingTree(
+                    destination,
+                    DocumentsContract.getTreeDocumentId(destination),
+                )
+
+                val newFileUri = DocumentsContract.createDocument(
+                    contentResolver,
+                    destinationFolderUri,
+                    EXCEL_MIME_TYPE,
+                    fileName,
+                )
+
+                if (newFileUri == null) return@withContext Result.failure(Exception("File uri is null."))
+
+                contentResolver.openOutputStream(newFileUri)
                     .use { workbook.write(it) }
 
-                Result.success(excelOutputFilePath.absoluteFile.toString())
+                Result.success(newFileUri.path.toString())
             } catch (exception: CancellationException) {
                 throw exception
             } catch (exception: IOException) {
@@ -42,5 +56,9 @@ class DefaultEventFileExporter @Inject constructor() : EventFileExporter {
     private fun generateFileName(): String {
         val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-hh-mm-ss")
         return "events-${dateTimeFormatter.format(LocalDateTime.now())}.xlsx"
+    }
+
+    companion object {
+        private const val EXCEL_MIME_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     }
 }
